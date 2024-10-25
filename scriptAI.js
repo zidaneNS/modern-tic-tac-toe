ad921e58ce14e0310fc9b35ebc7ad05281e0b68c
gadeeng <gadingkusuma99@gmail.com>
2024-10-25 09:54:48 +0700

    ubah dir

--- scriptAI.js
+++ scriptAI.js
@@ -18,13 +18,13 @@ class Player {
       this.color = "putih";
     }
     this.img = new Image();
-    this.img.src = `/assets/pion ${this.color}.png`;
+    this.img.src = `assets/pion ${this.color}.png`;
   }
   getPion() {
     if (this.pion > 0) {
       this.pion--;
       this.value = 1;
-      this.img.src = `/assets/pion ${this.color}.png`;
+      this.img.src = `assets/pion ${this.color}.png`;
     } else {
       this.canPion = false;
     }
@@ -34,7 +34,7 @@ class Player {
     if (this.knight > 0) {
       this.knight--;
       this.value = 2;
-      this.img.src = `/assets/knight ${this.color}.png`;
+      this.img.src = `assets/knight ${this.color}.png`;
     } else {
       this.canKnight = false;
     }
@@ -44,7 +44,7 @@ class Player {
     if (this.king) {
       this.king--;
       this.value = 3;
-      this.img.src = `/assets/king ${this.color}.png`;
+      this.img.src = `assets/king ${this.color}.png`;
     } else {
       this.canKing = false;
     }
